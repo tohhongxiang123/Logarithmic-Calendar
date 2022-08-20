db2fda41a703817045973a34eb59b89126bbf343
Toh Hong Xiang <tohhongxiang@gmail.com>
2022-08-21 03:33:54 +0800

    Feat: Change layout of app, improve DayViewer looks

--- components/DayView.tsx
+++ components/DayView.tsx
@@ -5,13 +5,31 @@ interface DayViewProps {
 export default function DayView({ date }: DayViewProps) {
     const hourlyTimes = [...Array(24).keys()].map(t => t < 10 ? `0${t}00` : `${t}00`)
 
+    const p = getPercentageOfDayPassed(date) * 100
+    console.log(p)
+
     return (
         <div className="flex flex-col relative">
             <ul className="flex flex-col">
+                <div className="border-b border-t border-red-500 bg-red-500 h-0 w-full absolute flex" style={{ top: `${p}%` }}>
+                    <div className="h-2 w-2 bg-red-500 -mt-1 -ml-1 rotate-45 rounded-sm" />
+                </div>
                 {hourlyTimes.map(time => (
-                    <li key={time} className="h-12 p-2 border-b border-gray-100">{time}</li>
+                    <li key={time} className="h-12 p-2 pb-4 border-b border-gray-100">{time}</li>
                 ))}
             </ul>
         </div>
     )
+}
+
+function getPercentageOfDayPassed(date: Date) {
+    const currentDate = new Date()
+    if (currentDate.toDateString() === date.toDateString()) {
+        const startOfDay = new Date()
+        startOfDay.setHours(0, 0, 0, 0)
+
+        return (currentDate.getTime() - startOfDay.getTime()) / (1000 * 60 * 60 * 24)
+    }
+
+    return -1
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Container, Indicator, useMantineTheme } from '@mantine/core'
-import { DatePicker } from '@mantine/dates'
+import { Container, Indicator, ScrollArea, useMantineTheme } from '@mantine/core'
+import { Calendar, DatePicker } from '@mantine/dates'
 import type { NextPage } from 'next'
 import { CSSProperties, useState } from 'react'
 import DayView from '../components/DayView'
@@ -9,28 +9,31 @@ import WeekView from '../components/WeekView'
 const Home: NextPage = () => {
 	const [value, setValue] = useState<Date>(new Date())
 	const theme = useMantineTheme();
+
 	return (
 		<div className="h-screen flex flex-col">
-			<div className={"p-8 border-b border-gray-200"}>
-				<DatePicker value={value} onChange={val => val && setValue(val)}
-					dropdownType="modal"
-					dayStyle={(date) =>
-						((date.toDateString() === new Date().toDateString()) && value.toDateString() !== new Date().toDateString()
-							? { backgroundColor: theme.colors.gray[2], fontWeight: 'bold', borderRadius: '100%' }
-							: null) as CSSProperties
-					}
-				/>
-			</div>
-			<div className="h-full grid grid-cols-3 overflow-hidden divide-x-2">
-				<div className="h-full overflow-scroll">
+			<div className="h-full flex overflow-hidden divide-x-2">
+				<div className={"p-8 border-b border-gray-200"}>
+					<Calendar value={value} onChange={val => val && setValue(val)}
+						dayStyle={(date) =>
+							((date.toDateString() === new Date().toDateString()) && value.toDateString() !== new Date().toDateString()
+								? { backgroundColor: theme.colors.gray[2], fontWeight: 'bold', borderRadius: '100%' }
+								: null) as CSSProperties
+						}
+					/>
+					<div className="mt-12 flex h-full w-full justify-center text-center">
+						<h2 className="text-2xl text-center"><strong>{value.toDateString()}</strong></h2>
+					</div>
+				</div>
+				<ScrollArea className="h-full flex-grow">
 					<DayView date={value} />
-				</div>
-				<div className="h-full overflow-scroll">
+				</ScrollArea>
+				<ScrollArea className="h-full flex-grow">
 					<WeekView date={value} />
-				</div>
-				<div className="h-full overflow-scroll">
+				</ScrollArea>
+				<ScrollArea className="h-full flex-grow">
 					<MonthView date={value} />
-				</div>
+				</ScrollArea>
 			</div>
 		</div>
 	)
