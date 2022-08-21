0b1af5162eeb289532ac464ef5bce4db34e914bb
Toh Hong Xiang <tohhongxiang@gmail.com>
2022-08-22 02:07:45 +0800

    Feat: prioritise mantine styles over tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
-import { MantineProvider } from '@mantine/core';
+import { createEmotionCache, MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <MantineProvider withGlobalStyles withNormalizeCSS><Component {...pageProps} /></MantineProvider>
+  return <MantineProvider        
+    withGlobalStyles
+    withNormalizeCSS
+    emotionCache={createEmotionCache({ key: 'mantine', prepend: false })} // prevent tailwindcss from overriding mantine styles
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
 }
 
 export default MyApp
