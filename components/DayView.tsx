7d6a15faaad15325040acabfaf80f14162470cfc
Toh Hong Xiang <tohhongxiang@gmail.com>
2022-08-22 01:18:00 +0800

    feature: Current time indicator updates in realtime

--- components/DayView.tsx
+++ components/DayView.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 interface DayViewProps {
     date: Date
 }
@@ -5,13 +7,19 @@ interface DayViewProps {
 export default function DayView({ date }: DayViewProps) {
     const hourlyTimes = [...Array(24).keys()].map(t => t < 10 ? `0${t}00` : `${t}00`)
 
-    const p = getPercentageOfDayPassed(date) * 100
-    console.log(p)
+    const [percentageOfDayPassed, setPercentageOfDayPassed] = useState(getPercentageOfDayPassed(date))
+    useEffect(() => {
+        const t = setInterval(() => {
+            setPercentageOfDayPassed(getPercentageOfDayPassed(date))
+        }, 1000 * 60) // update every minute
+        
+        return () => clearTimeout(t)
+    }, [])
 
     return (
         <div className="flex flex-col relative">
             <ul className="flex flex-col">
-                <div className="border-b border-t border-red-500 bg-red-500 h-0 w-full absolute flex" style={{ top: `${p}%` }}>
+                <div className="border-b border-t border-red-500 bg-red-500 h-0 w-full absolute flex" style={{ top: `${percentageOfDayPassed}%` }}>
                     <div className="h-2 w-2 bg-red-500 -mt-1 -ml-1 rotate-45 rounded-sm" />
                 </div>
                 {hourlyTimes.map(time => (
@@ -22,13 +30,20 @@ export default function DayView({ date }: DayViewProps) {
     )
 }
 
+/**
+ * 
+ * @param date date being shown on the calendar
+ * @returns percentage of day passed, a number between [0, 100] if `date` is today's, if not returns -1
+ * For example, if it is currently 21 aug 2022 1200hrs, and `date` is 21 aug 2022, returns 50 (%) of the day passed
+ * If it is currently 21 aug 2022 1200hrs, and `date` is 15 aug 2022, returns -1
+ */
 function getPercentageOfDayPassed(date: Date) {
     const currentDate = new Date()
     if (currentDate.toDateString() === date.toDateString()) {
         const startOfDay = new Date()
         startOfDay.setHours(0, 0, 0, 0)
 
-        return (currentDate.getTime() - startOfDay.getTime()) / (1000 * 60 * 60 * 24)
+        return (currentDate.getTime() - startOfDay.getTime()) / (1000 * 60 * 60 * 24) * 100
     }
 
     return -1
