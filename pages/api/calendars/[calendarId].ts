8524c77375ad6024f26012645616ed4d9f3b4516
Toh Hong Xiang <tohhongxiang@gmail.com>
2022-08-30 16:24:57 +0800

    Refactor api

--- pages/api/calendars/[calendarId].ts
+++ pages/api/calendars/[calendarId].ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
-import { CalendarEvent } from "../../types/Calendar";
+import { CalendarEvent } from "../../../types/Calendar";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const secret = process.env.NEXTAUTH_SECRET
@@ -8,7 +8,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!token) return res.status(401).json({ status: "No access token" })
     const accessToken = token.accessToken as string
 
-    const response = await fetch("https://www.googleapis.com/calendar/v3/calendars/tohhongxiang@gmail.com/events", {
+    const calendarId = req.query.calendarId
+    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {
         headers: { 'Authorization': `Bearer ${accessToken}` }
     }).then(res => res.json())
 
@@ -16,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const id = item.id
         const name = item.summary
         const start = new Date(item.start.dateTime ?? item.start.date)
-        const end = new Date(item.start.dateTime ?? item.start.date)
+        const end = new Date(item.end.dateTime ?? item.end.date)
 
         return { id, name, start, end }
     }) as CalendarEvent[]
